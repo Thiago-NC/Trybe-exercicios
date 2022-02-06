3bac1f46ea97f32f7aa28c086cdbc8454a382db4
Thiago-NC <thiagonc.84@gmail.com>
2022-02-06 19:50:21 -0300

    exer 2

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/Exer2.js
@@ -0,0 +1,15 @@
+//entrada
+let fruta = 'banana';
+let frutaInvertida = [];
+
+
+//processo
+for (let i = fruta.length; i >= 0; i -= 1){
+    frutaInvertida = fruta[i];
+    
+}
+
+console.log(frutaInvertida);
+
+//saida
+//palavra invertida
