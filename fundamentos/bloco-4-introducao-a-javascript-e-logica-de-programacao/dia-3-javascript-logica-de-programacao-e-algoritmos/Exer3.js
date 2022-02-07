b9e5ff3412559a3dcdeccbfd8203542c32755895
Thiago-NC <thiagonc.84@gmail.com>
2022-02-07 13:47:36 -0300

    Finalizado com ajuda

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/Exer3.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/Exer3.js
@@ -15,19 +15,38 @@
     //menor palavra
 */
 
-let array = ['java', 'javascript', 'python', 'html', 'css']
-let menorArray = '' ;
+let array = ['java','javascript','python', 'html', 'css']
 
-for (let i = 0; i < array.length; i+=1) {
-  menorArray = array[i] < array[i]
-  console.log (menorArray)
+let menorArray = array [0];
+let maiorArray = array[0];
+
+for (let i = 0; i < array.length; i+=1){
+  if (array[i].length < menorArray.length)
+  menorArray = array[i]
 }
+
+
+for (let i2 = 0; i2 < array.length; i2+=1){
+  if (array[i2].length > maiorArray.length) 
+  maiorArray = array[i2]     
+  }
+  console.log(menorArray)
+  console.log(maiorArray)
+
+
+
+/* for (let i = 0; i < array.length; i+=1) {
+    console.log (array[i])
+}
+   */
+  //console.log ()
+
   // menorArray = array[i] <= array
   
 
-
-    
-    
-
           
 //console.log(array[0])
+
+/* const arr = [14, 58, 20, 77, 66, 82, 42, 67, 42, 4]
+const min = Math.min(...arr)
+console.log(min) */
