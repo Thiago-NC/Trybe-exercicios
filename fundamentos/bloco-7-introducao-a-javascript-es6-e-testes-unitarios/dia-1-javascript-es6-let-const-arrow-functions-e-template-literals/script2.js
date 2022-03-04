adcf86ef5973af1d1b01971f7da19f80ee1afbc3
Thiago-NC <thiagonc.84@gmail.com>
2022-03-03 22:13:31 -0300

    exercicios part I

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script2.js
@@ -0,0 +1,4 @@
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+const returnOdds = oddsAndEvens.sort(function (a,b) {return a - b})
+
+console.log( `Os números ${returnOdds} se encontram ordenados de forma crescente!`);
