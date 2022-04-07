530ea97fb025ca71c3b2c9c2afe56dee1cab10dc
Thiago-NC <thiagonc.84@gmail.com>
2022-04-07 18:56:58 -0300

    commit atrasado

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/1script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/1script.js
@@ -61,13 +61,58 @@ const books = [
     },
   ];
   
-  // 1.Encontre o nome da primeira pessoa autora do livro nascida no ano de 1947
+/* 
+=====================================================================
+1.Encontre o nome da primeira pessoa autora do livro nascida no ano de 1947
+=====================================================================*/
   function authorBornIn1947(birthYear) {
      const teste = books.find((item) => item.author.birthYear === birthYear)
      return teste.author.name
   }
   
-  // const teste = books.find((item) => item.author.birthYear === 1947
-  // );
-  console.log(authorBornIn1947(1890));
+// const teste = books.find((item) => item.author.birthYear === 1947
+// );
+  // console.log(authorBornIn1947(1890));
   
+/* 
+=====================================================================
+2 - Retorne o nome do livro de menor nome. 
+=====================================================================
+*/
+const smallestName = () => {
+  const juntar = []
+  
+  books.forEach((item) => {
+    const allNames = item.name.length
+    juntar.push(allNames)
+  })
+
+  const min = Math.min(...juntar);
+  return min
+}
+// console.log(smallestName());
+
+/* 
+=====================================================================
+3- Encontre o primeiro livro cujo nome possui 26 caracteres.
+=====================================================================
+*/
+
+const findLetterNumber = () => {
+  
+  // let counter = []
+  books.find((index) => {
+    const allNames = index.name.length
+    // console.log(allNames)
+    if (allNames === 26) {
+      console.log(allNames)
+      index.name
+    }
+  });
+}
+  // if (countString === 26){
+    //   return counter.push
+    // }
+  // console.log(counter)
+    
+console.log(findLetterNumber())
---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/teste.js
@@ -0,0 +1,16 @@
+const arr = [14, 58, 20, 77, 66, 82, 42, 67, 42, 4]
+const min = Math.min(...arr)
+// console.log(min)
+
+const names = ['Eren', 'Naruto', 'Guts', 'Gon'];
+
+// const lower = names.length
+
+const teste = (array) => {
+    array.forEach((index) => {
+        return  console.log(index)
+    }
+)};
+
+// teste(names)
+console.log(teste(names))
